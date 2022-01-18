29872f6955c17e94823947c1d74164ad16fc7d0b
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-01-19 02:29:35 +0300

    package.json cleanup and Turkish ID Validator Implemented

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,50 @@
   <div class="container box">
     <div class="title">Welcome to E-Voting</div>
     <div class="subtitle">
-      <p>This is a simple voting system built with Vue.js.</p>
+      <p>This is a simple voting system built with Smart Contracts.</p>
       <p>You can vote for any of the candidates below.</p>
       <p>But please verify you identity first.</p>
     </div>
-    <form>
+    <form @submit.prevent="submit">
       <div class="field">
-        <label class="label">ID Number</label>
+        <label class="label">Please enter you ID Number</label>
         <div class="control">
-          <input class="input" type="text" placeholder="e.g. TC Kimlik" />
+          <input
+            class="input"
+            :class="userID ? valid.status ? 'is-success' : 'is-danger' : ''"
+            type="text"
+            v-model="userID"
+            placeholder="e.g. TC Kimlik"
+          />
+          <p v-if="!valid.status" class="help is-danger">{{ valid.display }}</p>
         </div>
       </div>
       <div class="control">
-        <router-link to="/verify" class="button is-link">Submit</router-link>
+        <button type="submit" :disabled="!valid.status" class="button is-link">Submit</button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
+import { checkId, generateId } from "turkish-id-checker";
 export default {
-  created() {
-
+  data() {
+    return {
+      userID: generateId(),
+    };
   },
+  computed: {
+    valid() {
+      return this.userID == '' ? true : checkId(this.userID);
+    },
+  },
+  methods: {
+    submit() {
+      if (this.userID && this.valid) {
+        this.$router.push("/verify");
+      }
+    },
+  }
 };
 </script>
