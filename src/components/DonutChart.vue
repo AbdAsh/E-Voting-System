2ecf11bd24f891242b7b7f6018d397f49d743084
AbdAsh <59181667+AbdAsh@users.noreply.github.com>
2022-01-19 14:31:16 +0300

    After Voting Chart

---
+++ src/components/DonutChart.vue
@@ -0,0 +1,43 @@
+<script>
+import { Doughnut } from "vue-chartjs";
+
+// export default {
+//   extends: Doughnut,
+//   data: () => ({
+//     chartdata: {
+//       labels: ['January', 'February'],
+//       datasets: [
+//         {
+//           label: 'Data One',
+//           backgroundColor: '#f87979',
+//           data: [40, 20]
+//         }
+//       ]
+//     },
+//     options: {
+//       responsive: true,
+//       maintainAspectRatio: false
+//     }
+//   }),
+
+//   mounted () {
+//     this.renderChart(this.chartdata, this.options)
+//   }
+// }
+export default {
+  name: "Doughnut",
+  extends: Doughnut,
+  props: ['chartData'],
+  data() {
+    return {
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.chartOptions);
+  },
+};
+</script>
--- src/views/PostVote.vue
+++ src/views/PostVote.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="container box">
-    <h1 class="title">Thank you for voting.</h1>
+  <div class="container">
+    <div class="box">
+      <h1 class="title">Thank you for voting.</h1>
+      <label class="label">Your vote has been recorded.</label>
+    </div>
+    <div v-if="this.candidates" class="box">
+      <label class="title">Current Statistics</label>
+      <DonutChart :chartData="chartData" />
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import DonutChart from "../components/DonutChart.vue";
+export default {
+  components: {
+    DonutChart,
+  },
+  computed: {
+    ...mapGetters("contracts", ["getContractData"]),
+    ...mapGetters("drizzle", ["drizzleInstance"]),
+    candidates() {
+      let data = this.getContractData({
+        contract: "Election",
+        method: "getCandidates",
+      });
+      if (data === "loading") return false;
+      return data.filter((e) => e[0] != 0);
+    },
+    chartData() {
+      let labels = [];
+      let datasets = [
+        {
+          label: [],
+          backgroundColor: [],
+          data: [],
+        },
+      ];
+      this.candidates ? this.candidates.map((e) => {
+        labels.push(e[1]);
+        datasets[0].label.push(e[1]);
+        datasets[0].data.push(e[2]);
+        datasets[0].backgroundColor.push("#" + Math.random().toString(16).slice(2, 8));
+      }) : null;
+      let processeedData = {
+        labels: labels,
+        datasets: datasets
+      };
+      return processeedData
+    },
+  },
+  created() {
+    this.$store.dispatch("drizzle/REGISTER_CONTRACT", {
+      contractName: "Election",
+      method: "getCandidates",
+      methodArgs: [],
+    });
+  },
+};
+</script>
